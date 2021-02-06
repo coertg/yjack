1a172b4527bf9f63fc1645960eeaf525310cfe60
Coert Grobbelaar <coertgrob@gmail.com>
2021-02-06 18:02:20 +0200

    Format output

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 const {url} = require('./src/command-line-args')
 const fetchInput = require('./src/fetch-input')
+const aWinsGame = require('./src/player-a-wins')
 
-fetchInput(url).then(result => {
-  console.log(url, result)
+fetchInput(url).then(games => {
+  //console.log(url, hands)
+
+  const results = games.map(game => {
+    try {
+      const result = aWinsGame(game.playerA, game.playerB)
+      return {...game, calculatedPlayerAWins: result, isCorrect: result === game.playerAWins}
+    } catch (e){
+      console.log('got error', e.message, game)
+      return {...game, error: e.message, isCorrect: false}
+    }
+  })
+  const failures = results.filter(r => r.playerAWins != r.calculatedPlayerAWins)
+  console.log(`${failures.length}/${games.length} incorrect`)
+  console.table(results, ['playerA', 'playerB', 'playerAWins', 'calculatedPlayerAWins', 'isCorrect'])
 })
